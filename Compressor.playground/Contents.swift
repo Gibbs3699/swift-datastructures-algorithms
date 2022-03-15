b8eee0c273cd073836aee1453c24e38607996a17
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-03-15 21:59:46 +0700

    Adding Compress

---
+++ Compressor.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+func compress(_ str: String) -> String {
+    var compressed = ""
+    var count = 0
+    let chars = Array(str)
+    
+    for i in 0..<str.count {
+        count += 1
+//        print(chars[i])
+        
+        if i+1 >= str.count || chars[i] != chars[i+1] {
+            compressed.append(chars[i])
+            compressed.append(String(count))
+            count = 0
+        }
+        
+    }
+    return compressed.count < str.count ? compressed : str
+}
+
+compress("aaabb")
+//compress("ccdd")
