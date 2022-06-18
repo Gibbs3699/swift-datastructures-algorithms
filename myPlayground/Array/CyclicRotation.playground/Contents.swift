4345db7a91083d53931b3fd04deec788b66cbb1a
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-06-18 23:54:32 +0700

    array: Adding CyclicRotation

---
+++ myPlayground/Array/CyclicRotation.playground/Contents.swift
@@ -0,0 +1,50 @@
+import UIKit
+/*
+ Rotate array to right N times.
+ https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/
+ 
+ For example, given
+
+     A = [3, 8, 9, 7, 6]
+     K = 3
+ the function should return [9, 7, 6, 3, 8]. Three rotations were made:
+
+     [3, 8, 9, 7, 6] -> [6, 3, 8, 9, 7]
+     [6, 3, 8, 9, 7] -> [7, 6, 3, 8, 9]
+     [7, 6, 3, 8, 9] -> [9, 7, 6, 3, 8]
+
+ */
+
+func solution(A: [Int], K: Int) -> [Int] {
+    guard !A.isEmpty else { return [] }
+    guard K > 0 else { return A }
+    
+    var result = A
+    
+    for _ in 1...K {
+        rotateRightOnce(A: result)
+    }
+    
+    return result
+}
+
+func rotateRightOnce(A: [Int]) -> [Int] {
+    var newArray = Array<Int>(repeating: 0, count: A.count)
+    
+    for index in 0..<A.count - 1 {
+       newArray[index + 1] = A[index]
+    }
+    newArray[0] = A.last!
+    
+    return newArray
+}
+
+rotateRightOnce(A: [1, 2, 3, 4, 5])
+
+solution(A: [1, 2, 3, 4, 5], K: 1) // 5 1 2 3 4
+solution(A: [1, 2, 3, 4, 5], K: 2) // 4 5 1 2 3
+solution(A: [1, 2, 3, 4, 5], K: 3) // 3 4 5 1 2
+
+solution(A: [3, 8, 9, 7, 6], K: 3) // [9, 7, 6, 3, 8]
+
+
